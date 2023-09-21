0e675ada287b9490c720ac4a0c9ed154f51675d9
l.oppi <oppi.lucas@gmail.com>
2023-09-20 21:38:15 -0300

    Initial Commit

---
+++ include/ADE78X.h
@@ -0,0 +1,12 @@
+#ifndef ADE78X_H
+#define ADE78X_H
+
+/* 
+IC Datasheet:
+https://www.mouser.com/datasheet/2/609/ADE7854A_7858A_7868A_7878A-534151.pdf
+at 20/09/2023 21:36 GMT-3
+*/
+
+
+
+#endif /* ADE78X_H */
